eb3ae7108b0c7e7f388fcf26b1cf8c40f1797482
Hadley Wickham <h.wickham@gmail.com>
2011-03-01 00:00:00 +0000

    version 0.1

---
+++ R/lal.r
@@ -0,0 +1,8 @@
+#' Los Angeles Lakers play-by-play data.
+#' 
+#' Play by play data from the 28/10/2008 basketball game of the Los Angeles
+#' Lakers vs. the Portland Trail Blazers.
+#' 
+#' @docType data
+#' @name lal
+NULL
